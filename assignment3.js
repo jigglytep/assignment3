702366b38fa85394c71767e4feb25c028003dc8b
Lev Tsenovoy <lev.tsenovoy@ibm.com>
2022-11-07 10:52:31 -0500

    forgot to commit changes

--- assignment3.js
+++ assignment3.js
@@ -31,6 +31,8 @@ const question2 = (myValue) => {
   /////// write your IF statement below this line /////////
   if (myValue.length == 10) {
     myLang = "Javascript";
+  } else {
+    myLang = "Python";
   }
   /////// write your IF statement above this line /////////
   return myLang;
@@ -45,7 +47,13 @@ const question2 = (myValue) => {
 const question3 = (myValue) => {
   let myLang;
   /////// write your IF statement below this line /////////
-
+  if (myValue.length == 10) {
+    myLang = "Javascript";
+  } else if (myValue.length > 3) {
+    myLang = "PHP";
+  } else {
+    myLang = "Python";
+  }
   /////// write your IF statement above this line /////////
   return myLang;
 };
@@ -59,7 +67,23 @@ const question3 = (myValue) => {
 const question4 = (myNumber) => {
   let myFaveFood;
   /////// write your SWITCH statement below this line /////////
-
+  switch (expression) {
+    case 1:
+      myFaveFood = "Pizza";
+      break;
+    case 2:
+      myFaveFood = "Hamburger";
+      break;
+    case 3:
+      myFaveFood = "Ice Cream";
+      break;
+    case 4:
+      myFaveFood = "Sushi";
+      break;
+    default:
+      myFaveFood = "Pho";
+      break;
+  }
   /////// write your SWITCH statement above this line /////////
   return myFaveFood;
 };
@@ -75,7 +99,7 @@ const question4 = (myNumber) => {
 const question5 = (myTemp) => {
   let myWeather;
   /////// write your TERNARY OPERATOR statement below this line /////////
-
+  myWeather = myTemp >= 75 ? "Great weather!" : "Still cold.";
   /////// write your TERNARY OPERATOR statement above this line /////////
   return myWeather;
 };
